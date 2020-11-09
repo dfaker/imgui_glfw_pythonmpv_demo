dbc31eb15a7828c50418dd08cb8df39c71bfcca0
dfaker <35278260+dfaker@users.noreply.github.com>
2020-11-09 13:58:38 +0000

    Add files via upload

--- main.py
+++ main.py
@@ -10,95 +10,158 @@ from imgui.integrations.glfw import GlfwRenderer
 import ctypes
 from mpv import MPV, MpvRenderContext, OpenGlCbGetProcAddrFn
 
-def main():
-    imgui.create_context()
-    ctx,player,window = impl_glfw_init()
-    impl = GlfwRenderer(window)
+class VideoPlayer:
+
+  def terminate(self):
+    self.ctx.free()
+    self.mpv.terminate()
+    
+
+    
+
+  def __init__(self,filename):
+    self.filename = filename
+    self.fbo = None
+    self.open = True
 
 
-    _   = gl.glGenFramebuffers(1)
-    fbo = gl.glGenFramebuffers(1)
-    playbackPos = (0,)
-    volume = (0,)
+  def init(self):
 
-    gl.glBindFramebuffer(gl.GL_FRAMEBUFFER, fbo)
-
-
-    texture = gl.glGenTextures(1)
-    gl.glBindTexture(gl.GL_TEXTURE_2D, texture)
+    self.fbo = gl.glGenFramebuffers(1)
+    gl.glBindFramebuffer(gl.GL_FRAMEBUFFER, self.fbo)
+    
+    self.texture = gl.glGenTextures(1)
+    gl.glBindTexture(gl.GL_TEXTURE_2D, self.texture)
     
     gl.glTexParameteri(gl.GL_TEXTURE_2D, gl.GL_TEXTURE_MIN_FILTER, gl.GL_LINEAR)
     gl.glTexParameteri(gl.GL_TEXTURE_2D, gl.GL_TEXTURE_MAG_FILTER, gl.GL_LINEAR)
 
-    gl.glFramebufferTexture2D(gl.GL_FRAMEBUFFER, gl.GL_COLOR_ATTACHMENT0, gl.GL_TEXTURE_2D, texture, 0)
+    gl.glFramebufferTexture2D(gl.GL_FRAMEBUFFER, gl.GL_COLOR_ATTACHMENT0, gl.GL_TEXTURE_2D, self.texture, 0)
+    
+    gl.glTexImage2D(gl.GL_TEXTURE_2D, 0, gl.GL_RGB, 100, 100, 0, gl.GL_RGB, gl.GL_UNSIGNED_BYTE, None);
 
+    gl.glBindTexture(gl.GL_TEXTURE_2D, 0)
     gl.glBindFramebuffer(gl.GL_FRAMEBUFFER, 0)
 
+    self.playbackPos = (0,)
+    self.volume = (0,)
+    self.loop='inf'
+
+    self.mpv = MPV(log_handler=print, loglevel='debug')
+
+    def get_process_address(_, name):
+      print(name)
+      address = glfw.get_proc_address(name.decode('utf8'))
+      return ctypes.cast(address, ctypes.c_void_p).value
+
+
+    proc_addr_wrapper = OpenGlCbGetProcAddrFn(get_process_address)
+
+    self.ctx = MpvRenderContext(self.mpv, 'opengl', opengl_init_params={'get_proc_address': proc_addr_wrapper})
+    
+    self.mpv.play(self.filename)
+    self.mpv.volume=0
+
+  def render(self):
+
+
+
+    videowindow, self.open = imgui.begin("Video window {}".format(self.filename), self.open)
+
+    if self.fbo is None:
+      self.init()
+
+    w,h = imgui.core.get_content_region_available()
+    w=int(max(w,0))
+    h=int(max(h-80,0))
+
+    if not self.open:
+      imgui.end()
+      self.terminate()
+      return
+
+    if self.ctx.update() and w>0 and h>0:
+
+      gl.glBindFramebuffer(gl.GL_FRAMEBUFFER, self.fbo)
+      gl.glBindTexture(gl.GL_TEXTURE_2D, self.texture);
+      
+      gl.glTexImage2D(gl.GL_TEXTURE_2D, 0, gl.GL_RGB, w, h, 0, gl.GL_RGB, gl.GL_UNSIGNED_BYTE, None);
+
+      gl.glBindFramebuffer(gl.GL_FRAMEBUFFER, 0)
+      gl.glBindTexture(gl.GL_TEXTURE_2D, 0);
+
+      self.ctx.render(flip_y=False, opengl_fbo={'w': w, 'h': h, 'fbo': self.fbo})
+
+    try:
+      imgui.text("Filename: {} fbo: {} tex: {}".format(self.mpv.filename,self.fbo,self.texture))
+      imgui.text("{0:.2f}s/{1:.2f}s ({2:.2f}s remaining)".format(self.mpv.time_pos,self.mpv.duration,self.mpv.playtime_remaining))
+    except:
+      pass
+
+    imgui.image(self.texture, w,h )
+    
+
+
+    changed, values = imgui.slider_float(
+        "Playback Percentage", *self.playbackPos,
+        min_value=0.0, max_value=100.0,
+        format="%.0f",
+        power=1.0
+    )
+
+    if changed and values:
+      self.mpv.command('seek',values,'absolute-percent')
+      self.playbackPos = (values,)
+    elif self.mpv.percent_pos:
+      self.playbackPos = (self.mpv.percent_pos,)
+
+
+    changed, values = imgui.slider_float(
+        "Volume", *self.volume,
+        min_value=0.0, max_value=100.0,
+        format="%.0f",
+        power=1.0
+    )
+
+    if changed:
+      self.mpv.volume = values
+      self.volume = (values,)
+    elif self.mpv.volume:
+      self.volume = (self.mpv.volume,)
+    imgui.end()
+
+def main():
+    imgui.create_context()
+    window = impl_glfw_init()
+    impl = GlfwRenderer(window)
+    _   = gl.glGenFramebuffers(1)
+
+    videoWindows     = []
+    
+    def dropFile(window,files):
+      for file in files:
+        videoWindows.append(VideoPlayer(file))
+
+    glfw.set_drop_callback(window, dropFile)
 
     while not glfw.window_should_close(window):
+
         glfw.poll_events()
         impl.process_inputs()
 
         imgui.new_frame()
 
-        if imgui.begin_main_menu_bar():
-            if imgui.begin_menu("File", True):
-
-                clicked_quit, selected_quit = imgui.menu_item(
-                    "Quit", 'Cmd+Q', False, True
-                )
-
-                if clicked_quit:
-                    exit(1)
-
-                imgui.end_menu()
-            imgui.end_main_menu_bar()
-
-        imgui.begin("Video window", True, flags=imgui.WINDOW_NO_SCROLLBAR)
-
-        w,h = imgui.core.get_content_region_available()
-        w=int(max(w,0))
-        h=int(max(h-80,0))
-
-        imgui.text("Filename: {}".format(player.filename))
-        imgui.text("{0:.2f}s/{1:.2f}s ({2:.2f}s remaining)".format(player.time_pos,player.duration,player.playtime_remaining))
-
-        if ctx.update() and w>0 and h>0:
-          gl.glBindTexture(gl.GL_TEXTURE_2D, texture);
-          gl.glTexImage2D(gl.GL_TEXTURE_2D, 0, gl.GL_RGB, w, h, 0, gl.GL_RGB, gl.GL_UNSIGNED_BYTE, None);
-          ctx.render(flip_y=False, opengl_fbo={'w': w, 'h': h, 'fbo': fbo})
-        imgui.image(fbo, w, h)
-
-        changed, values = imgui.slider_float(
-            "Playback Percentage", *playbackPos,
-            min_value=0.0, max_value=100.0,
-            format="%.0f",
-            power=1.0
-        )
-
-        if changed:
-          player.command('seek',values,'absolute-percent')
-          playbackPos = (values,)
+        videoWindows = [x for x in videoWindows if x.open]
+        if len(videoWindows) > 0:
+          for videoWindow in videoWindows:
+            videoWindow.render()
         else:
-          playbackPos = (player.percent_pos,)
-          
+          imgui.core.set_next_window_position(10,10)
+          imgui.core.set_next_window_size(500,10)
 
-
-        changed, values = imgui.slider_float(
-            "Volume", *volume,
-            min_value=0.0, max_value=100.0,
-            format="%.0f",
-            power=1.0
-        )
-
-        if changed:
-          player.volume = values
-          volume = (values,)
-        else:
-          volume = (player.volume,)
-
-
-        imgui.end()
+          imgui.begin("##Message", True, flags=imgui.WINDOW_NO_SCROLLBAR|imgui.WINDOW_NO_RESIZE|imgui.WINDOW_NO_TITLE_BAR|imgui.WINDOW_NO_MOVE )
+          imgui.text("Drop one or more video files onto this window to play")
+          imgui.end()
 
         gl.glClearColor(0., 0., 0., 1.)
         gl.glClear(gl.GL_COLOR_BUFFER_BIT)
@@ -108,7 +171,8 @@ def main():
         glfw.swap_buffers(window)
 
 
-    player.terminate()
+    for videoWindow in videoWindows:
+      videoWindow.terminate()
     impl.shutdown()
     glfw.terminate()
 
@@ -149,14 +213,7 @@ def impl_glfw_init():
     ctx = None
     mpv = MPV(log_handler=print, loglevel='debug')
     
-
-    ctx = MpvRenderContext(mpv, 'opengl', opengl_init_params={'get_proc_address': proc_addr_wrapper})
-
-    
-    mpv.play('sample.mp4')
-    mpv.volume=0
-
-    return ctx,mpv,window
+    return window
 
 
 if __name__ == "__main__":
